fae436e517533ba8d1705655dccb00cf57b4a3a1
raminassar87 <raminassar2000@hotmail.com>
2018-10-29 08:48:58 +0400

    Update JavawyBackendApplication.java

--- src/main/java/com/javawy/backend/javawybackend/JavawyBackendApplication.java
+++ src/main/java/com/javawy/backend/javawybackend/JavawyBackendApplication.java
@@ -3,21 +3,20 @@ package com.javawy.backend.javawybackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @SpringBootApplication
+@RequestMapping("message")
+@RestController
 public class JavawyBackendApplication {
 
-    @GetMapping("/")
+    @GetMapping("/hi")
     public String sayHi() {
         return "Welcome to Heroku Cloud solution";
     }
-
-    @GetMapping("/message/{input}")
-    public String getMessage(@PathVariable String input) {
-        return "Welcome " + input;
-    }
-
+    
     public static void main(String[] args) {
         SpringApplication.run(JavawyBackendApplication.class, args);
     }
